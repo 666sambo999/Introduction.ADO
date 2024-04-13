5cff2c82c132738e69cffff8f620c4ab05c081aa
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2024-04-13 12:16:23 +0500

    Forms

---
+++ Libery3/MainForm1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Libery3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxQiery = new System.Windows.Forms.RichTextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBoxQiery
+            // 
+            this.richTextBoxQiery.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxQiery.Location = new System.Drawing.Point(22, 12);
+            this.richTextBoxQiery.Multiline = false;
+            this.richTextBoxQiery.Name = "richTextBoxQiery";
+            this.richTextBoxQiery.Size = new System.Drawing.Size(574, 23);
+            this.richTextBoxQiery.TabIndex = 0;
+            this.richTextBoxQiery.Text = "";
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExecute.Location = new System.Drawing.Point(620, 12);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 1;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(673, 386);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 451);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.richTextBoxQiery);
+            this.Name = "MainForm";
+            this.Text = "Libery3";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxQiery;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Libery3/MainForm1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace Libery3
+{
+    public partial class MainForm : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+        SqlDataReader reader;
+        DataTable table;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            connection = new SqlConnection(connectionString);
+            MessageBox.Show(this, connectionString, "ConnectionString", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            richTextBoxQiery.SelectAll();
+            richTextBoxQiery.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            string cmdLine = richTextBoxQiery.Text;
+            SqlCommand cmd = new SqlCommand(cmdLine, connection);
+            connection.Open();
+            reader = cmd.ExecuteReader();
+            table = new DataTable();
+            for(int i = 0; i < reader.FieldCount;i++ )table.Columns.Add(reader.GetName(i)); 
+            while(reader.Read()) 
+            { 
+                DataRow row = table.NewRow();
+                for (int i = 0; i < reader.FieldCount; i++) row[i] = reader[i];
+                table.Rows.Add(row);
+            }
+            dataGridView1.DataSource = table;
+
+            connection.Close(); 
+        }
+    }
+}
